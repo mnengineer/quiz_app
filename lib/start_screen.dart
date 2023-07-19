afa81ffbfb9a520dbeb2cdb12c9191c5c89b9f52
mnengineer <masashiii0115@gmail.com>
2023-07-19 23:08:08 +0900

    feat: ボタンの透過度設定

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -12,7 +12,15 @@ class StartScreen extends StatelessWidget {
           Image.asset(
             'assets/images/quiz-logo.png',
             width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
           ),
+          // Opacity(
+          //   opacity: 0.5,
+          //   child: Image.asset(
+          //     'assets/images/quiz-logo.png',
+          //     width: 300,
+          //   ),
+          // ),
           const SizedBox(height: 80),
           const Text(
             'Learn Flutter the fun way!',
